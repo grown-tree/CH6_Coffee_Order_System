e2f662d9b4a683deb663a6647c9284be04ede0dc
“grown-tree” <tjsruddks12@naver.com>
2026-05-07 11:24:20 +0900

    first commit

---
+++ src/main/java/com/example/coffee_order_system/CoffeeOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffee_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOrderSystemApplication.class, args);
+    }
+
+}
